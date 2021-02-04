ee6d7cc235d1c167c9b9131bec4ec2307eafa84c
jeiea <solum5013@gmail.com>
2021-02-04 22:49:46 +0900

    feat: make example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,58 +9,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Hook example',
+        home: HookBuilder(builder: (_) {
+          final value = useCustom();
+          return Scaffold(
+            body: Center(
+                child: Column(mainAxisSize: MainAxisSize.min, children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('count: $value'),
+              )
+            ])),
+          );
+        }));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+useCustom() {
+  return use(_Custom());
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _Custom extends Hook<int> {
+  const _Custom();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+  _CustomState createState() => _CustomState();
+}
+
+class _CustomState extends HookState<int, _Custom> {
+  @override
+  int build(BuildContext context) {
+    final s = useState(5);
+    return s.value;
   }
 }
