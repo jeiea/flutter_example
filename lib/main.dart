568f06fbe99bf5a6c53a585c9b958cb7657dfc91
jeiea <solum5013@gmail.com>
2021-04-10 01:16:21 +0900

    feat: add example for animation

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,26 @@ void main() {
   runApp(MyApp());
 }
 
+Route slideRoute(
+    {RouteSettings? settings, required Widget Function() builder}) {
+  return PageRouteBuilder(
+    settings: settings,
+    pageBuilder: (_, __, ___) => builder(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+        SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(1, 0),
+        end: Offset.zero,
+      ).animate(CurvedAnimation(
+          parent: animation,
+          curve: Curves.easeOutExpo,
+          reverseCurve: Curves.easeInExpo)),
+      child: child,
+    ),
+    transitionDuration: const Duration(milliseconds: 500),
+  );
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,53 +32,79 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        if (settings.name == '/') {
+          return PageRouteBuilder(
+            settings: settings,
+            pageBuilder: (_, __, ___) => MyHomePage(),
+            transitionDuration: Duration(milliseconds: 0),
+            reverseTransitionDuration: Duration(milliseconds: 0),
+          );
+        }
+        if (settings.name == '/second') {
+          return slideRoute(settings: settings, builder: () => MyHomePage());
+        }
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final points = <String>[];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+
+    (() async {
+      for (int i = 0; i < 4; i++) {
+        await Future.delayed(const Duration(milliseconds: 100));
+        setState(() {
+          recordPoint('future');
+        });
+      }
+    })();
+  }
+
+  void recordPoint(String prefix) {
+    final route = ModalRoute.of(context);
+    final value = route?.animation?.value;
+    if (value != null) {
+      points.add('$prefix $value');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final route = ModalRoute.of(context);
+
+    recordPoint('build');
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('animation should'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            if (route?.settings.name == '/')
+              ElevatedButton(
+                onPressed: () async {
+                  await Navigator.pushNamed(context, '/second');
+                },
+                child: Text('Start transition'),
+              ),
+            ...points.map((x) => Text(x)).toList(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
