ab7c7abf0a96e1e3b8dfe1e874e7c66cdb689176
jeiea <solum5013@gmail.com>
2021-05-07 15:06:54 +0900

    feat: add mockito example

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,21 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bug_report/main.dart';
+import 'package:mockito/annotations.dart';
 
+import 'widget_test.mocks.dart';
+
+@GenerateMocks(const [HttpClient])
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    final client = MockHttpClient();
+    client.get('', 80, '');
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
