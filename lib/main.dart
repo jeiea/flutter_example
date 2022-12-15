6364258da071207ed7bbe42d8d917700fb2f58e5
jeiea <solum5013@gmail.com>
2022-12-15 18:18:34 +0900

    feat: useEffect question report

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends HookWidget {
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final tapped = useState<bool?>(null);
+    final logs = useState([]);
+
+    useEffect(() {
+      if (tapped.value == true) {
+        // logs.value = [...logs.value, 'useEffect tap'];
+        print('mount 1: ${logs.value}');
+      } else if (tapped.value == false) {
+        // logs.value = [...logs.value, 'useEffect tap 2'];
+        print('mount 2: ${logs.value}');
+      }
+      return () {
+        // logs.value = [...logs.value, 'useEffect unmount'];
+        print('unmount: ${logs.value}');
+      };
+    }, [tapped.value != true]);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('tap');
+                    tapped.value = tapped.value != true;
+                  },
+                  child: Text('Tap me'),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: logs.value.length,
+                  itemBuilder: ((context, index) => Text(logs.value[index])),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
